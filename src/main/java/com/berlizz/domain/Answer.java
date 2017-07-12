7b6e940e326920402165e3f1371e57755d97206b
berlizz <44berlizz@gmail.com>
2017-07-12 20:24:41 +0900

    질문 상세보기, 수정, 삭제, 답변 추가 기능 구현

--- src/main/java/com/berlizz/domain/Answer.java
+++ src/main/java/com/berlizz/domain/Answer.java
@@ -1,8 +1,6 @@
 package com.berlizz.domain;
 
 import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
-import java.util.List;
 
 import javax.persistence.Entity;
 import javax.persistence.ForeignKey;
@@ -11,62 +9,43 @@ import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.Lob;
 import javax.persistence.ManyToOne;
-import javax.persistence.OneToMany;
-import javax.persistence.OrderBy;
 
 @Entity
-public class Question {
+public class Answer {
 
 	@Id  // primary key 지정
 	@GeneratedValue  // 자동으로 1씩 증가(auto_increment)
 	private Long id;
-
+	
 	@ManyToOne
-	@JoinColumn(foreignKey = @ForeignKey(name="fk_question_writer"))
+	@JoinColumn(foreignKey = @ForeignKey(name="fk_answer_writer"))
 	private User writer;
 	
-	private String title;
+	@ManyToOne
+	@JoinColumn(foreignKey = @ForeignKey(name="fk_answer_to_question"))
+	private Question question;
 	
-	@OneToMany(mappedBy="question")
-	@OrderBy("id ASC")
-	private List<Answer> answers;  // 답변의 목록을 question 이 직접 가지고 있을 수 있음
-	
-	@Lob  // 255자 제한을 없애버림
+	@Lob
 	private String contents;
 	
 	private LocalDateTime createTime;
 	
-	public Question() {
+	public Answer() {
 		// jpa에서 mapping 할 때 인자를 받는 생성자가 있고 이것처럼 default 생성자를 적어줘야함
 	}
 	
-	public Question(User writer, String title, String contents) {
+	public Answer(User writer, Question question, String contents) {
 		super();
 		this.writer = writer;
-		this.title = title;
+		this.question = question;
 		this.contents = contents;
 		this.createTime = LocalDateTime.now();
 	}
 
-	public String getFormattedCreateTime() {
-		if(createTime == null) {
-			return null;
-		}
-		
-		return createTime.format(DateTimeFormatter.ofPattern("yyyy.MM.dd HH:mm:ss"));
-	}
-	
-	public int getCountedAnswers() {
-		return answers.size();
-	}
-
-	public void update(String title, String contents) {
-		this.title = title;
-		this.contents = contents;
-	}
-
-	public boolean isSameWriter(User loginUser) {	
-		return this.writer.equals(loginUser);
+	@Override
+	public String toString() {
+		return "Answer [id=" + id + ", writer=" + writer + ", contents=" + contents + ", createTime=" + createTime
+				+ "]";
 	}
 
 	@Override
@@ -85,7 +64,7 @@ public class Question {
 			return false;
 		if (getClass() != obj.getClass())
 			return false;
-		Question other = (Question) obj;
+		Answer other = (Answer) obj;
 		if (id == null) {
 			if (other.id != null)
 				return false;
---
+++ src/main/java/com/berlizz/domain/AnswerRepository.java
@@ -0,0 +1,7 @@
+package com.berlizz.domain;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AnswerRepository extends JpaRepository<Answer, Long> {
+
+}
